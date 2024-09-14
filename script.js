5cccb4be71ee8c9f3e65ce3a04a6231e4a9be20b
is44co <aboodymain1@gmail.com>
2024-09-14 23:23:36 +0300

    edit

---
+++ script.js
@@ -0,0 +1,64 @@
+const Added = document.querySelector(".inputs");
+
+
+
+const data = document.querySelector(".data");
+
+const btn = document.querySelector(".AddBtn");
+const deleted=document.querySelector('.delete')
+deleted.addEventListener('click',()=>{
+   const lists=document.querySelectorAll('.list').forEach((li)=>{
+    li.remove()
+    }
+)
+const fonte=document.querySelectorAll('.fa-solid').forEach((li)=>{
+    li.remove()
+    }
+) 
+    
+    })
+
+btn.addEventListener('click',()=>{
+    if(data.value===''){
+        return
+    }
+    if(data.value.trim()===''){
+        return
+    }
+
+const created=document.createElement('input');
+created.readOnly=true;
+
+Added.appendChild(created);
+created.classList.add('list')
+console.log(data.value )
+const fontAwesome=document.createElement('i')
+fontAwesome.classList.add('fa-solid','fa-pen-to-square');
+Added.appendChild(fontAwesome)
+
+const fontAwesome2=document.createElement('i')
+fontAwesome2.classList.add('fa-solid','fa-trash');
+Added.appendChild(fontAwesome2)
+
+
+created.value=data.value.trim()
+data.value='';
+fontAwesome.addEventListener('click',()=>{
+    created.readOnly=false;
+    created.addEventListener('mouseleave',()=>{
+        created.readOnly=true;
+    
+    
+    })
+
+})
+fontAwesome2.addEventListener('click',()=>{
+    created.remove()
+    fontAwesome.remove()
+    fontAwesome2.remove()
+})
+
+})
+
+
+
