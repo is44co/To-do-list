595c482316b796e1a361b90ac8b7567db110bd8f
is44co <aboodymain1@gmail.com>
2024-09-15 23:14:20 +0300

    ed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Added = document.querySelector(".inputs");
-
+const form = document.querySelector('form')
 
 
 const data = document.querySelector(".data");
@@ -18,47 +18,49 @@ const fonte=document.querySelectorAll('.fa-solid').forEach((li)=>{
     
     })
 
-btn.addEventListener('click',()=>{
-    if(data.value===''){
-        return
-    }
-    if(data.value.trim()===''){
-        return
-    }
-
-const created=document.createElement('input');
-created.readOnly=true;
-
-Added.appendChild(created);
-created.classList.add('list')
-console.log(data.value )
-const fontAwesome=document.createElement('i')
-fontAwesome.classList.add('fa-solid','fa-pen-to-square');
-Added.appendChild(fontAwesome)
-
-const fontAwesome2=document.createElement('i')
-fontAwesome2.classList.add('fa-solid','fa-trash');
-Added.appendChild(fontAwesome2)
-
-
-created.value=data.value.trim()
-data.value='';
-fontAwesome.addEventListener('click',()=>{
-    created.readOnly=false;
-    created.addEventListener('mouseleave',()=>{
+    function logSubmit(e) {
+        e.preventDefault();  
+    
+        if(data.value===''){
+            return
+        }
+        if(data.value.trim()===''){
+            return
+        }
+    
+        const created=document.createElement('input');
         created.readOnly=true;
     
+        Added.appendChild(created);
+        created.classList.add('list')
+        console.log(data.value )
+        
+        const fontAwesome=document.createElement('i')
+        fontAwesome.classList.add('fa-solid','fa-pen-to-square');
+        Added.appendChild(fontAwesome)
     
-    })
+        const fontAwesome2=document.createElement('i')
+        fontAwesome2.classList.add('fa-solid','fa-trash');
+        Added.appendChild(fontAwesome2)
+    
+        created.value=data.value.trim()
+        data.value='';
+        
+        fontAwesome.addEventListener('click',()=>{
+            created.readOnly=false;
+            created.addEventListener('mouseleave',()=>{
+                created.readOnly=true;
+            })
+        })
+        
+        fontAwesome2.addEventListener('click',()=>{
+            created.remove()
+            fontAwesome.remove()
+            fontAwesome2.remove()
+        })
+    }
+    
 
-})
-fontAwesome2.addEventListener('click',()=>{
-    created.remove()
-    fontAwesome.remove()
-    fontAwesome2.remove()
-})
-
-})
 
 
-
+form.addEventListener("submit", logSubmit);
